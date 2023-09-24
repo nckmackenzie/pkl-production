90c1307b910d7bed3107d1d640146ada743c9e2e
nicmackenzie <nicmackenzie@live.com>
2023-09-24 09:54:09 +0300

    add types for jobcard and options

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
 type UserType = 'Admin' | 'User';
 
+type JobStatus = 'ongoing' | 'paused' | 'completed';
+
 type Department = {
   id: number;
   text: string;
@@ -11,4 +13,23 @@ type User = {
   password: string;
   departmentId: number | null;
   role: UserType;
+};
+
+type JobCard = {
+  id: number;
+  jobcardNo: string;
+  client: string;
+  description: string;
+  cumulativeHrs: number;
+  hoursInProduction?: number;
+  currentDepartment?: string;
+  startedOn: Date;
+  status: JobStatus;
+  completedDate?: Date;
+  salesPerson: string;
+};
+
+type Options = {
+  label: string;
+  value: string;
 };
