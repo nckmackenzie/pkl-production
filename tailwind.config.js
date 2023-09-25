db205643ee024495dea8db4a0e3e9854d06cd425
nicmackenzie <nicmackenzie@live.com>
2023-09-25 14:49:28 +0300

    styling for multiselect

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './node_modules/react-tailwindcss-select/dist/index.esm.js',
   ],
   theme: {
     container: {
