e972cb28fbf7f834369c163cb62a27f330d0c6f8
nicmackenzie <nicmackenzie@live.com>
2023-09-25 21:53:36 +0300

    render task lists

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,3 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '@/features/authentication/useUser';
+import { useEffect } from 'react';
+import TaskAction from '@/features/tasks/TaskAction';
+import TasksList from '@/features/tasks/TasksList';
+
 export default function Tasks() {
-  return <div>Tasks</div>;
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  useEffect(
+    function () {
+      if (user?.role === 'User') {
+        navigate('/tasks/' + user?.userId);
+      }
+    },
+    [navigate, user?.role, user?.userId]
+  );
+  return (
+    <div className="space-y-12">
+      <TaskAction />
+      <TasksList />
+    </div>
+  );
 }
