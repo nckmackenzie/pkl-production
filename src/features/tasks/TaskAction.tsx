0579455a5a3ff04a494817e37bfc9a99ef458bfc
nicmackenzie <nicmackenzie@live.com>
2023-09-25 21:54:10 +0300

    task actions

---
+++ src/features/tasks/TaskAction.tsx
@@ -0,0 +1,42 @@
+import Filter from '@/components/ui/filter';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { FILTEROPTIONS } from '../jobcards/constants';
+
+export default function TaskAction() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="w-1/2 grid grid-cols-12 items-end gap-x-2">
+        <div className="col-span-4 p-1">
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Filter By Dept" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="yard">Yard</SelectItem>
+              <SelectItem value="machinery">Machinery</SelectItem>
+              <SelectItem value="joinery">Joinery</SelectItem>
+              <SelectItem value="carving">Carving</SelectItem>
+              <SelectItem value="sanding">Sanding</SelectItem>
+              <SelectItem value="polishing">Polishing</SelectItem>
+              <SelectItem value="upholstery">Upholstery</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="col-span-4 p-1">
+          <Input placeholder="or search by job card" />
+        </div>
+      </div>
+      <Filter
+        filterKey="status"
+        options={FILTEROPTIONS.filter(option => option.value !== 'completed')}
+      />
+    </div>
+  );
+}
