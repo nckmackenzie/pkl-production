77d60c72b16f384ee5ac39ff9cc0e22eaf6752a0
nicmackenzie <nicmackenzie@live.com>
2023-09-25 11:48:35 +0300

    add util functions for date and percentage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import { formatDistance } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -6,3 +7,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const PAGE_SIZE = 10;
+
+export function formatDateFromDistance(date: Date) {
+  return formatDistance(date, new Date(), {
+    addSuffix: true,
+  });
+}
+
+export function getPercentage(x: number, y: number) {
+  return Math.round((x / y) * 100);
+}
