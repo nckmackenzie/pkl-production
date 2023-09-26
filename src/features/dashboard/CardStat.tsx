f50172cc1bd4bb618a5df0997df7f49df7318123
nicmackenzie <nicmackenzie@live.com>
2023-09-26 12:37:54 +0300

    add size prop

--- src/features/dashboard/CardStat.tsx
+++ src/features/dashboard/CardStat.tsx
@@ -2,6 +2,8 @@ import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { LucideIcon } from 'lucide-react';
 
+type sizes = 'sm' | 'default';
+
 type CardStatProps = {
   className?: string;
   title: string;
@@ -9,6 +11,7 @@ type CardStatProps = {
   icon: LucideIcon;
   bgColor: string;
   textColor: string;
+  size?: sizes;
 };
 
 export default function CardStat({
@@ -18,21 +21,40 @@ export default function CardStat({
   icon: Icon,
   bgColor,
   textColor,
+  size = 'default',
 }: CardStatProps) {
   return (
     <Card className={cn('rounded-md p-2', className)}>
       <div className="flex items-center justify-between">
         <div>
-          <h4 className="text-lg text-primary">{title}</h4>
-          <div className="text-4xl font-semibold">{value}</div>
+          <h4
+            className={cn(
+              'text-primary',
+              size === 'default' ? 'text-lg' : 'text-base'
+            )}
+          >
+            {title}
+          </h4>
+          <div
+            className={cn(
+              'font-semibold',
+              size === 'default' ? 'text-4xl' : 'text-lg'
+            )}
+          >
+            {value}
+          </div>
         </div>
         <div
           className={cn(
-            'border w-12 h-12 flex items-center justify-center rounded-full bg-green-500/10',
-            bgColor
+            'border flex items-center justify-center rounded-full bg-green-500/10',
+            `${bgColor} ${size === 'default' ? 'w-12 h-12' : 'w-8 h-8'}`
           )}
         >
-          <Icon className={cn('w-8 h-8', textColor)} />
+          <Icon
+            className={`${textColor} ${
+              size === 'default' ? 'w-8 h-8' : 'w-4 h-4'
+            }`}
+          />
         </div>
       </div>
     </Card>
